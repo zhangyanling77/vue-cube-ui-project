8704ce4ce9afcf76d5da663af1ba9d9520e42ebf
zhangyanling77 <18783226594@163.com>
2019-11-04 16:47:26 +0800

    feature: add apis

--- src/api/home.js
+++ src/api/home.js
@@ -1,7 +1,24 @@
 import axios from '@/utils/ajaxRequest'
+import store from '@/store'
 
+// 获取课程分类
 export const fetchCategory = () => {
   return axios.request({
     url: '/category'
+  })
+}
+
+// 获取轮播图
+export const fetchSlides = () => {
+  return axios.request({
+    url: '/slides'
+  })
+}
+
+// 获取列表对应的数据
+export const fetchLessonList = (size, offset) => {
+  let { state: { home: { currentLesson } } } = store
+  return axios.request({
+    url: `/lessonList/${currentLesson}?size=${size}&offset=${offset}`
   })
 }
