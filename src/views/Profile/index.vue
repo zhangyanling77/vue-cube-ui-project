433294326e97c50b7b993a4391a6d1e57f900c2c
zhangyanling77 <18783226594@163.com>
2019-11-05 11:08:59 +0800

    Update index.vue

--- src/views/Profile/index.vue
+++ src/views/Profile/index.vue
@@ -1,3 +1,16 @@
 <template>
-  <div>profile</div>
+  <div>
+    <cube-button primary @click="toLogin">登录</cube-button>
+  </div>
 </template>
+
+
+<script>
+export default {
+  methods: {
+    toLogin() {
+      this.$router.push("/login");
+    }
+  }
+};
+</script>
