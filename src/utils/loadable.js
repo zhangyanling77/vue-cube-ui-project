36c2233bfff1846984bcb06a524fa59e7e2174ae
zhangyanling77 <18783226594@163.com>
2019-11-06 14:33:17 +0800

    Update loadable.js

--- src/utils/loadable.js
+++ src/utils/loadable.js
@@ -2,16 +2,15 @@
 import Loading from '@/components/Loading'
 const loadable = (asyncFunction) => {
   const component = () => ({
-    component: asyncFunction(),
+    component: asyncFunction(), // 得到异步加载的组件
     loading: Loading
   })
 
-  // 最终要返回一个组件，组件需要有render，通过render 在去渲染一个异步组件
-  return { // cli 默认不支持模板
+  return { // cli 默认不支持模板，所有使用render，而不是template
     render(h) {
       return h(component)
     }
   }
-  // 组件是一个对象 会变成render函数
 }
+
 export default loadable
