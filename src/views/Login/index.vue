245ffbff6c1b7f322a553a45d17828025bbb7579
zhangyanling77 <18783226594@163.com>
2019-11-08 10:26:42 +0800

    feature:重写登录组件

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,20 +1,44 @@
 <template>
-  <div>
-    <cube-form :model="model" @submit="submitHandler">
-      <cube-form-group>
-        <cube-form-item :field="fields[0]"></cube-form-item>
-        <cube-form-item :field="fields[1]"></cube-form-item>
-      </cube-form-group>
-      <cube-form-group>
-        <cube-button type="submit">Submit</cube-button>
-      </cube-form-group>
-    </cube-form>
+  <div class="login wrapper">
+    <MHeader>登录</MHeader>
+    <div class="login-form">
+      <img src="../../assets/images/login_bg1.png" alt />
+      <cube-form :model="model" @submit="submitHandler">
+        <cube-form-group>
+          <cube-form-item :field="fields[0]"></cube-form-item>
+          <cube-form-item :field="fields[1]"></cube-form-item>
+        </cube-form-group>
+        <cube-form-group>
+          <cube-button type="submit">登录</cube-button>
+        </cube-form-group>
+      </cube-form>
+    </div>
   </div>
 </template>
+<style lang="stylus">
+.login {
+  &-form {
+    width: 80%;
+    margin: 0 auto;
+
+    img {
+      width: 100px;
+      height: 100px;
+      margin: 0 auto;
+      display: block;
+      margin-bottom: 40px;
+    }
+  }
+}
+</style>
 <script>
 import { mapActions } from "vuex";
+import MHeader from '@/components/MHeader'
 import * as types from "@/store/actions-type";
 export default {
+  components:{
+    MHeader
+  },
   data() {
     return {
       model: {
@@ -52,7 +76,13 @@ export default {
     ...mapActions([types.LOGIN]),
     submitHandler(e) {
       e.preventDefault();
-      this[types.LOGIN](this.model);
+      try{
+        // 登录
+        this[types.LOGIN](this.model);
+        this.$router.push('/')
+      }catch(e){
+        console.log(e);
+      }
     }
   }
 };
