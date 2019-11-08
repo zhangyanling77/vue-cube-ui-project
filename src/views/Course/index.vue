d1563852fe71fa32812fd1fc64ef4708c520652f
zhangyanling77 <18783226594@163.com>
2019-11-08 10:25:08 +0800

    Update index.vue

--- src/views/Course/index.vue
+++ src/views/Course/index.vue
@@ -1,3 +1,20 @@
 <template>
   <div>course</div>
 </template>
+
+<script>
+export default {
+  // beforeRouteEnter(to,from,next){ // 路由守卫 只有通过路由渲染的才会执行这些钩子
+  //   console.log('路由进入前');
+  //   next((vm)=>{
+  //       console.log(vm);
+  //   })
+  // },
+  // beforeRouteUpdate(){
+
+  // },
+  // beforeRouteLeave(){
+
+  // }
+}
+</script>
