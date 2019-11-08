a7f506b779de4ee8da4dfc740b3d1caeccccee51
zhangyanling77 <18783226594@163.com>
2019-11-08 10:06:36 +0800

    feature:添加身份验证

--- src/router/hook.js
+++ src/router/hook.js
@@ -12,15 +12,15 @@ export default {
   },
 
   // 给每一个hook 对应一个功能 可以做权限校验相关的
-  permission: (to, from, next) => {
+  permission: async (to, from, next) => {
     // 如果用户没有登录 访问了课程页面 应该跳转登录页
 
     // 1、要知道用户是否登录 2、要知道哪个页面需要登录才能看
     let needLogin = to.matched.some(item => item.meta.needLogin)
     // 这里可以给一个白名单
 
-    if(!store.state.hasPermission){
-      let flag = await store.dispatch(types.VALIDATE)
+    if(!store.state.hasPermission){ // 看是否有权限
+      let flag = await store.dispatch(types.VALIDATE) // 验证是否登录过
       if(needLogin){
         if(!flag){
           next('/login')
@@ -47,6 +47,20 @@ export default {
   },
   // 在页面切换的时候 需要拿到 当前状态是否登录
   profileAuth: function(to, from, next){
-    // 明天继续
+    // 
+    if(store.state.hashPermission && store.state.user.menuList){
+       if(!store.state.menuPermission){
+         let list = store.state.user.menuList.map(item => item .auth)
+         let newRoutes = auth.filter(item => list.includes(item.name))
+         // newRoutes就是我们要新增的路由
+         store.commit(types.SET_MENU_LIST)
+         this.adddRoutes(newRoutes)
+         next({...to, replace: true})
+       } else{
+         next()
+       }
+    } else{
+      next()
+    }
   }
 }
